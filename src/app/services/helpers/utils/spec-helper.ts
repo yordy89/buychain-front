c8d54ede3f0223e0eeac7d2b41bac98a245b914e
Yordanys Albelo <yordanys.albelo@towersoft.net>
2022-12-20 19:03:38 -0500

    update package

--- src/app/services/helpers/utils/spec-helper.ts
+++ src/app/services/helpers/utils/spec-helper.ts
@@ -1,16 +1,18 @@
 import { Environment } from '@services/app-layer/app-layer.environment';
-import { SpecLib } from '@buychain/lib';
+//import { SpecLib } from '@buychain/lib';
 import { ProductSpec } from '@services/data-layer/http-api/base-api/swagger-gen';
 
 export class SpecHelper {
   public static getSpecShorthand(spec: ProductSpec): string {
     const uiProducts = Environment.getUiProducts();
     if (!uiProducts) return '';
-    return SpecLib.getShorthand(spec, uiProducts) || '';
+    //return SpecLib.getShorthand(spec, uiProducts) || '';
+    return '';
   }
 
   public static substituteTemplate(template, specs) {
-    return SpecLib._generateShorthand(template, specs);
+    //return SpecLib._generateShorthand(template, specs);
+    return '';
   }
 
   public static getUomSymbol(uom: string) {
